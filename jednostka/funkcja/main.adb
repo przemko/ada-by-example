b1f13c99df96eb208917dacf44e6866c8e9f8a8b
Przemysław Kobylański <przemko@mac.com>
2017-03-04 10:44:51 +0100

    dodano przykład

---
+++ jednostka/funkcja/main.adb
@@ -0,0 +1,7 @@
+with Square;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+procedure Main is
+begin
+   Put (Square (10));
+end Main;
---
+++ jednostka/funkcja/square.adb
@@ -0,0 +1,4 @@
+function Square (N : Integer) return Integer is
+begin
+   return N * N;
+end Square;
